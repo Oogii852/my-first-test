38a763279c88a54bf8068df654fbbd0013de7d04
Oogii852 <otgonsukh0311@gmail.com>
2026-09-16 22:12:42 +0800

    Enable trace, video and screenshot in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,13 +23,13 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    video: 'on',
+    screenshot: 'on',
   },
 
   /* Configure projects for major browsers */
@@ -65,7 +65,7 @@ export default defineConfig({
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
     // {
-    //   name: 'Google Chrome',
+    //   name: 'Google Chrome',  
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
